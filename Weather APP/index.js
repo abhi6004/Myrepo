caa59a0632d8bfa3ab1a72c7e67485230030260e
abhi6004 <abhiradadiya30012004@gmail.com>
2023-03-03 15:07:59 +0530

    update project

--- Weather APP/index.js
+++ Weather APP/index.js
@@ -1,51 +1,7 @@
-console.log("hello world");
+const userTab = document.querySelector("[data-userWeather]")
+const searchTab = document.querySelector("[data-searchWeather]")
+const userContainer = document.querySelector("[.weather-container]")
 
-const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
-
-function renderWeatherInfo() {
-        let newPara = document.createElement('p');
-        newPara.textContent = `${data?.main?.temp.toFixed(2)} *C`
-
-        document.body.appendChild(newPara); 
-        }
-
-
-async function fetchWeatherDetails() {
-        
-        try{
-            let city = "surat";
-
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-        const data = await response.json();8
-    
-        console.log("Weather data:-> " , data);
-
-        renderWeatherInfo(data);
-        }
-        
-        catch(error) {
-        //hendele data   
-        }
-    
-}
-
-async function getCurstomWetherdata() {
-    try{
-        let latitude = 17;
-        let longitude = 18;
-
-        let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?
-        lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-
-        let data = result.json();
-
-        console.log(data);
-
-        renderWeatherInfo(data);
-    }
-
-    catch(error){
-        console.log("error is comeing" , error);
-    }
-
-}
+const grantAccessContainer = document.querySelector(".grand-loction-container");
+const loadingScreen = document.querySelector(".loading-container");
+const 
